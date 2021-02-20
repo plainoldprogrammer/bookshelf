98710d1c75379d5625645b63af500aae3adaf76a
plainoldprogrammer <plainoldprogrammer@gmail.com>
2021-02-20 15:00:31 -0600

    creating a new book record on the database

--- ef/Program.cs
+++ ef/Program.cs
@@ -6,7 +6,16 @@ namespace ef
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dbContext = new DbBookshelfContext();
+
+            var book = new Book
+            {
+                Title = "The C Programming Language",
+                Author = "Dennis Ritchie and Brian Kernighan"
+            };
+
+            dbContext.Add<Book>(book);
+            dbContext.SaveChanges();
         }
     }
 }

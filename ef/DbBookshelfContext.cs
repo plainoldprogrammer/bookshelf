49ae41a0c964f15a2d4b87ee205ad3c4707a9972
plainoldprogrammer <plainoldprogrammer@gmail.com>
2021-02-20 14:35:44 -0600

    scaffolding the database

--- ef/DbBookshelfContext.cs
+++ ef/DbBookshelfContext.cs
@@ -17,17 +17,24 @@ namespace ef
         {
         }
 
+        public virtual DbSet<Book> Books { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("DataSource=..\\\\DbBookshelf");
+                optionsBuilder.UseSqlite("DataSource=..\\\\DbBookshelf.db");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
 
